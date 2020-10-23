ff89dc93767abe6ba08da8f79a6c5e754f0e3b51
mohitparekh7 <54593876+mohitparekh7@users.noreply.github.com>
2020-10-23 21:30:57 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<p>this is index page</p>
+	<a href="signup.php">Register page</a>
+	<a href="login.php">Login page</a>
+</body>
+</html>
