fb7b0b004728b791839e80996350282e014328e0
mohitparekh7 <54593876+mohitparekh7@users.noreply.github.com>
2020-10-28 23:18:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,77 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Bay View</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<style type="text/css">
+
+		.navbar{
+			background-color: white;
+			background-image: linear-gradient(to right top, #ff471a 0%, #ffb366 100%);
+		}
+		.navbar-brand{
+			color: black;
+			font-size: 24px;
+			font-family: "Times New Roman", Times, serif;
+
+		}
+
+		.navigation a:hover{
+			color: #4a4a4a;
+		}
+
+		.links a{
+			font-size: 17px;
+			color: black;
+		}
+
+
+	</style>
 </head>
 <body>
-	<p>this is index page</p>
-	<a href="signup.php">Register page</a>
-	<a href="login.php">Login page</a>
+	<nav class="navbar navbar-expand-lg">
+		<div class="container navigation">
+		  <a class="navbar-brand" href="#">Bay View Cafe</a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+
+
+		  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+			    <ul class="navbar-nav ml-auto">
+			      <li class="nav-item active px-2 links">
+			        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+			      </li>
+
+			      <li class="nav-item px-2 links">
+			        <a class="nav-link" href="#">Menu</a>
+			      </li>
+
+			      <li class="nav-item px-2 links">
+			        <a class="nav-link" href="#">About Us</a>
+			      </li>
+
+			      <li class="nav-item px-2 links">
+			        <a class="nav-link" href="contactus.php">Contact Us</a>
+			      </li>
+
+			      <li class="nav-item px-2 links">
+			        <a class="nav-link" href="login.php">Log in</a>
+			      </li>
+
+			      <li class="nav-item px-2 links">
+			        <a class="nav-link" href="signup.php">Sign Up</a>
+			      </li>
+
+			    </ul>
+		  	</div>
+		 </div>
+	</nav>
+
+	<h1>This is INDEX PAGE</h1>
 </body>
 </html>
