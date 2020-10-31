f1c90f4f9b227f10888c6edabdf4b2f09b4eb3dd
mohitparekh7 <54593876+mohitparekh7@users.noreply.github.com>
2020-10-31 05:44:27 +0530

    Delete connect.php

--- connect.php
+++
@@ -1,4 +0,0 @@
-<?php 
-	$db = mysqli_connect('localhost','root','','food_order') or die('Unable to connect. Check your connection parameters.');
-	mysqli_select_db($db,'food_order') or die(mysqli_error($db))
-?>
